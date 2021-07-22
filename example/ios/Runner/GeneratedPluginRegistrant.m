c8cffaa7eb59edec60d00d29d50e7b378d764513
Lawati97 <37277245+Lawati97@users.noreply.github.com>
2021-07-22 15:51:28 +0400

    Add files via upload

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_elliptic_curve_keypair/FlutterEllipticCurveKeypairPlugin.h>)
+#import <flutter_elliptic_curve_keypair/FlutterEllipticCurveKeypairPlugin.h>
+#else
+@import flutter_elliptic_curve_keypair;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterEllipticCurveKeypairPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterEllipticCurveKeypairPlugin"]];
+}
+
+@end
---
+++ lib/models/ecc_result_model.dart
@@ -0,0 +1,11 @@
+class EccResultModel {
+  String data;
+  bool success;
+  String description;
+ //This was changed required was added
+  EccResultModel({
+    required this.data,
+    required this.success,
+    required this.description,
+  });
+}
